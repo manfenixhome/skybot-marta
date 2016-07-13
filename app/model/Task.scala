98a13acf4477937d61ba613539fc6c8177a4dbe7
Anton Maniskevych <anton@mybeautymatches.com>
2016-07-13 17:21:55 +0300

    some changes

--- app/model/Task.scala
+++ app/model/Task.scala
@@ -22,10 +22,10 @@ object Task {
   val tasks = Seq(
     //    Task(1, "You will working from home tomorrow?", 10, 15, new DateTime(), Seq("-Yes", "-No")),
     //      Task(2, "Sorry for disturb you, could you please write your lunch for tomorrow? \nhttps://goo.gl/DhwCDt\nThanks", 0 , 30, new DateTime(), Seq())
-    Task(1, "Working from home", "You will working tomorrow from home?", 10, 60 * 60 * 24 * 7 /*7 days*/, calcFirstDay(DateTimeConstants.WEDNESDAY, 12,0, 7), Seq("-Yes", "-No")),
-    Task(2, "Food hacker lunch", "Sorry for disturb you, could you please write your lunch for tomorrow? \nhttps://goo.gl/DhwCDt\nThanks", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 14,0, 1), Seq()),
-    Task(3, "Razvozka", "Sorry for disturb you, only want to reminding you don't forget booking seats for your journey to work or home? \nhttps://goo.gl/VXDzFk\nThanks", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 13,30, 1), Seq()),
-    Task(4, "Track your time", "I know you going to go home, only want to reminding you don't forget track your time in Redmine.\nThanks and have a lovely evening (hug) ", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 14,55, 1), Seq())
+    /*0*/Task(1, "Working from home", "You will working tomorrow from home?", 10, 60 * 60 * 24 * 7 /*7 days*/, calcFirstDay(DateTimeConstants.WEDNESDAY, 12,0, 7), Seq("-Yes", "-No")),
+    /*1*/Task(2, "Food hacker lunch", "Sorry for disturb you, could you please write your lunch for tomorrow? \nhttps://goo.gl/DhwCDt\nThanks", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 14,0, 1), Seq()),
+    /*2*/Task(3, "Razvozka", "Sorry for disturb you, only want to reminding you don't forget booking seats for your journey to work or home? \nhttps://goo.gl/VXDzFk\nThanks", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 13,30, 1), Seq()),
+    /*3*/Task(4, "Track your time", "I know you going to go home, only want to reminding you don't forget track your time in Redmine.\nThanks and have a lovely evening (hug) ", 0 , 60 * 60 * 24 /*every day*/, calcFirstDay(new DateTime().getDayOfWeek, 14,55, 1), Seq())
   )
 
   def calcFirstDay(dayOfWeek: Int, hour: Int, minutes: Int, delayInDays: Int): DateTime = {
