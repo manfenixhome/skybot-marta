6602be8572c7653ad4cf859a55a7a6380236dad6
Anton Maniskevych <anton@mybeautymatches.com>
2016-07-21 13:31:26 +0300

    Fixed working from home. Added new texts for lunch ordering

--- app/services/TaskScheduleService.scala
+++ app/services/TaskScheduleService.scala
@@ -29,15 +29,30 @@ class TaskScheduleService @Inject()(actorSystem: ActorSystem, sendService: SendM
       val today = new DateTime().getDayOfWeek
       if (today != DateTimeConstants.SATURDAY && today != DateTimeConstants.SUNDAY) {
         val users = db.getUsersByTaskId(task.id)
-        users.foreach(user => sendService.sendMessage(user, "%s \n%s".format(task.message, task.answers.mkString("\n"))))
+        launchTask(task, users, today)
       }
+    }
+  }
+
+  private def launchTask(task: Task, users: Seq[String], today: Int): Unit = {
+    if (task.id == 2) {
+      val targetDay = today match {
+        case DateTimeConstants.THURSDAY => "Monday"
+        case DateTimeConstants.FRIDAY => "Tuesday"
+        case DateTimeConstants.MONDAY => "Wednesday"
+        case DateTimeConstants.TUESDAY => "Thursday"
+        case DateTimeConstants.WEDNESDAY => "Friday"
+        case _ => ""
+      }
+      users.foreach(user => sendService.sendMessage(user, "%s \n%s".format(task.message.format(targetDay), task.answers.mkString("\n"))))
+    } else {
+      users.foreach(user => sendService.sendMessage(user, "%s \n%s".format(task.message, task.answers.mkString("\n"))))
     }
   }
 
   def launchTask(task: Task): Unit = {
     println("admin started task=" + task.title)
-    val users = db.getUsersByTaskId(task.id)
-    users.foreach(user => sendService.sendMessage(user, "%s \n%s".format(task.message, task.answers.mkString("\n"))))
+    launchTask(task, Seq("8:antonekreative"), DateTimeConstants.FRIDAY)
     sendService.sendMessage("8:antonekreative", "Sent")
   }
 
